93cd83b6d6f4d362b601ae172d16bad5d751260c
Ritik Bharti <ritikbharti4388@gmail.com>
2022-01-27 17:56:21 +0530

    Update InsertionSort.cpp

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -1,3 +1,12 @@
+// In Insertion sort array is virtually split into a sorted and an unsorted part. Values from the unsorted part are picked
+// and placed at the correct position in the sorted part.
+// Algorithm 
+// To sort an array of size n in ascending order: 
+// 1: Iterate from arr[1] to arr[n] over the array. 
+// 2: Compare the current element (key) to its predecessor. 
+// 3: If the key element is smaller than its predecessor, compare it to the elements before. Move the greater elements
+// one position up to make space for the swapped element.
+
 //using dynamic array.
 
 #include<iostream>
