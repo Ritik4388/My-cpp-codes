4c6f874c1e292330d947ad0f9c1c391a0489a66e
Ritik Bharti <ritikbharti4388@gmail.com>
2022-05-15 12:00:04 +0530

    Delete Documents/React/login directory

--- Documents/React/login/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./app.jsx";
-
-const isUserRegistered = true;
-
-ReactDOM.render(<App />, document.getElementById("root"));
